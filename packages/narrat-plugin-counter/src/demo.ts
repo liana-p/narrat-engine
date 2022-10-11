f44f4b9f4c96227ecd625c41bce9937ea017af39
Liana Pigeot <32243840+liana-pigeot@users.noreply.github.com>
2022-10-11 21:08:40 +0100

    another ts error

--- packages/narrat-plugin-counter/src/demo.ts
+++ packages/narrat-plugin-counter/src/demo.ts
@@ -5,7 +5,6 @@ import { CounterPlugin } from './plugin';
 window.addEventListener('load', () => {
   registerPlugin(new CounterPlugin());
   startApp({
-    charactersPath: 'data/characters.json',
     configPath: 'data/config.json',
     logging: false,
     debug: true,
