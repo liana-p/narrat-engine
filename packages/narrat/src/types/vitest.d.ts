d6233c1b05cef71419939b7fa9249726768997e3
Liana P <liana.pigeot@gmail.com>
2023-04-10 15:05:24 +0100

    fixed build command issue

---
+++ packages/narrat/src/types/vitest.d.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/ban-types */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import type { TestingLibraryMatchers } from '@testing-library/jest-dom/matchers';
+
+// declare global {
+//   namespace jest {
+//     interface Matchers<R = void> extends TestingLibraryMatchers<typeof expect.stringContaining, R> {}
+
+//   }
+// }
