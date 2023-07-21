7e6250177772b276c85d5457ed263421a130613d
Liana <32243840+liana-p@users.noreply.github.com>
2023-07-21 21:29:38 +0100

    feat: video portraits, hidden portraits, custom size portraits (#133)
    * feat: adding video portraits support and also hidden portraits
    
    * feat: video portraits, hidden portraits, custom size portraits

--- packages/narrat/src/components/dialog-picture.vue
+++ packages/narrat/src/components/dialog-picture.vue
@@ -1,55 +1,82 @@
 <template>
   <div class="dialog-picture override" :style="boxStyle">
-    <img :src="getAssetUrl(pictureUrl!)" class="picture override" />
+    <img
+      :src="getAssetUrl(getCharacterPicUrl(picture?.image))"
+      class="picture override"
+      v-if="picture"
+    />
+    <video
+      v-if="video"
+      class="picture override"
+      :autoplay="video.autoplay === false ? false : true"
+      :loop="video.loop === false ? false : true"
+      :muted="video.muted ? true : false"
+    >
+      <source :src="getAssetUrl(getCharacterPicUrl(video.video))" />
+    </video>
   </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import { getConfig, getAssetUrl, getDialogPanelWidth } from '@/config';
 import { defaultConfig } from '@/config/config-output';
 import { useRenderingStore } from '@/stores/rendering-store';
-import { mapState } from 'pinia';
-import { defineComponent } from 'vue';
-export default defineComponent({
-  props: {
-    pictureUrl: String,
-  },
-  methods: {
-    getAssetUrl(url: string) {
-      return getAssetUrl(url);
-    },
-  },
-  computed: {
-    ...mapState(useRenderingStore, ['layoutMode']),
-    boxStyle(): any {
-      const rendering = useRenderingStore();
-      const layout = getConfig().layout;
-      let right: any = 0;
-      let bottom: any = 0;
-      const portrait = layout.portraits;
-      if (this.layoutMode === 'vertical') {
-        const portraitMode = portrait.offset?.portrait ?? {
-          right: 0,
-          bottom: 0,
-        };
-        right = 20 + portraitMode.right;
-        bottom = rendering.dialogHeight + portraitMode.bottom;
-      } else {
-        const landscape = portrait.offset?.landscape ?? { right: 0, bottom: 0 };
-        const panelOffset =
-          getConfig().dialogPanel.rightOffset ??
-          defaultConfig.dialogPanel.rightOffset;
-        right = getDialogPanelWidth() - 10 + landscape.right + panelOffset;
-        bottom = 200 + landscape.bottom;
-      }
-      return {
-        right: `${right}px`,
-        bottom: `${bottom}px`,
-        width: `${layout.portraits.width}px`,
-        height: `${layout.portraits.height}px`,
-      };
-    },
-  },
+import { computed } from 'vue';
+import {
+  ImageCharacterPose,
+  VideoCharacterPose,
+} from '@/config/characters-config';
+import { getCharacterPicUrl } from '@/utils/characters';
+
+const props = defineProps<{
+  picture: ImageCharacterPose | undefined;
+  video: VideoCharacterPose | undefined;
+}>();
+
+const video = computed(() => {
+  return props.video;
+});
+
+const layoutMode = computed(() => {
+  return useRenderingStore().layoutMode;
+});
+const boxStyle = computed(() => {
+  const rendering = useRenderingStore();
+  const layout = getConfig().layout;
+  let right: any = 0;
+  let bottom: any = 0;
+  const portrait = layout.portraits;
+  if (layoutMode.value === 'vertical') {
+    const portraitMode = portrait.offset?.portrait ?? {
+      right: 0,
+      bottom: 0,
+    };
+    right = 20 + portraitMode.right;
+    bottom = rendering.dialogHeight + portraitMode.bottom;
+  } else {
+    const landscape = portrait.offset?.landscape ?? { right: 0, bottom: 0 };
+    const panelOffset =
+      getConfig().dialogPanel.rightOffset ??
+      defaultConfig.dialogPanel.rightOffset;
+    right = getDialogPanelWidth() - 10 + landscape.right + panelOffset;
+    bottom = 200 + landscape.bottom;
+  }
+  let width = layout.portraits.width;
+  let height = layout.portraits.height;
+  if (props.video) {
+    width = props.video.width ?? width;
+    height = props.video.height ?? height;
+  }
+  if (props.picture) {
+    width = props.picture.width ?? width;
+    height = props.picture.height ?? height;
+  }
+  return {
+    right: `${right}px`,
+    bottom: `${bottom}px`,
+    width: `${width}px`,
+    height: `${height}px`,
+  };
 });
 </script>
 
@@ -62,6 +89,7 @@ export default defineComponent({
   border-radius: 10px;
   background-color: grey;
   z-index: 99;
+  transition: all 0.5s ease-in-out;
 }
 
 .dialog-picture img {
