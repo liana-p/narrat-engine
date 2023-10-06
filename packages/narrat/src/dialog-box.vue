0c5e1fd0c9a334331409ab99c3998525409c2356
Liana Pigeot <32243840+liana-p@users.noreply.github.com>
2023-10-06 18:18:49 +0100

    fix: switch to using the returnType of timeout to see if CI is happy. still not sure why build behaves differently in CI than locally...

--- packages/narrat/src/dialog-box.vue
+++ packages/narrat/src/dialog-box.vue
@@ -73,12 +73,13 @@ import { getCharacterStyle } from './utils/characters';
 import { findAllHtmlTags } from './utils/string-helpers';
 import { useNavigation } from './inputs/useNavigation';
 import { InputListener, useInputs } from '@/stores/inputs-store';
+import { Interval, Timeout } from '@/utils/time-helpers';
 
 export interface TextAnimation {
   text: string;
   index: number;
   startTime: number;
-  timer: NodeJS.Timer | null;
+  timer: Interval | null;
   skippedChars: number;
   tags: RegExpExecArray[];
   finished: boolean;
@@ -90,9 +91,9 @@ const passed = ref(false);
 const timeout = ref<any>(null);
 const textAnimation = ref<TextAnimation | null>(null);
 const mounted = ref(false);
-const autoTimer = ref<NodeJS.Timer | null>(null);
-const skipTimer = ref<NodeJS.Timer | null>(null);
-const nextLineTimer = ref<NodeJS.Timer | null>(null);
+const autoTimer = ref<Timeout | null>(null);
+const skipTimer = ref<Timeout | null>(null);
+const nextLineTimer = ref<Timeout | null>(null);
 const playerInput = ref<HTMLInputElement | null>(null);
 const textFieldInputGrabber = ref<InputListener | null>(null);
 
