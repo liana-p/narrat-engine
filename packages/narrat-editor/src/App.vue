c3b4d5644b22f6f068f61a9c79ed9c35ee3506c2
Liana P <32243840+liana-p@users.noreply.github.com>
2023-10-15 11:20:18 +0100

    feat: basic setup for editor

---
+++ packages/narrat-editor/src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import MonacoEditor from './components/MonacoEditor.vue';
+
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <h1 class="title">Narrat Editor!</h1>
+    <div class="editor-layout flex justify-between">
+      <MonacoEditor class="grow"/>
+      <div class="in-between grow-0"></div>
+      <div class="game-preview grow">
+        <h2>Game preview</h2>
+      </div>
+    </div>
+  </div>
+  <!-- <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" /> -->
+</template>
+
+<style scoped>
+
+</style>
---
+++ packages/narrat-editor/src/components/MonacoEditor.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div ref="editor-container">
+
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import monaco from 'monaco-editor';
+
+export type MonacoEditor = ReturnType<typeof monaco.editor.create>;
+const editor = ref<MonacoEditor | null>(null);
+const editorContainer = ref<HTMLDivElement | null>(null);
+
+onMounted(() => {
+  editor.value = monaco.editor.create(editorContainer.value!, {
+    automaticLayout: true,
+    language: 'python',
+    value: `main:
+  talk player idle "Hello, world!"`
+  });
+})
+</script>
+
+<style scoped>
+
+</style>
