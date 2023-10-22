c658bb469081c9a0596579cec4192e618bca6f90
Liana Pigeot <32243840+liana-p@users.noreply.github.com>
2023-10-22 17:16:38 +0100

    fix: theme included in narrat editor

--- packages/narrat-editor/src/themes/jrpg/index.ts
+++ packages/narrat-editor/src/themes/jrpg/index.ts
@@ -5,24 +5,26 @@ export const jrpgTheme: Theme = {
   id: 'jrpg',
   css: JrpgCss,
   extendedConfig: {
-    dialogPanel: {
-      width: 1200,
-      height: 300,
-      rightOffset: 50,
-      bottomOffset: 50,
-    },
-    layout: {
-      portraits: {
-        width: 200,
+    common: {
+      dialogPanel: {
+        width: 1200,
         height: 300,
-        offset: {
-          landscape: {
-            right: -700,
-            bottom: 200,
-          },
-          portrait: {
-            right: 10,
-            bottom: 0,
+        rightOffset: 50,
+        bottomOffset: 50,
+      },
+      layout: {
+        portraits: {
+          width: 200,
+          height: 300,
+          offset: {
+            landscape: {
+              right: -700,
+              bottom: 200,
+            },
+            portrait: {
+              right: 10,
+              bottom: 0,
+            },
           },
         },
       },
