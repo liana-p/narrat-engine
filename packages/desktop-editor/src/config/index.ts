190f96f6524a6c9b102c092c5521ea56421ee2a1
Liana Pigeot <32243840+liana-p@users.noreply.github.com>
2024-01-22 16:36:19 +0000

    feat: narrat game running in tauri

---
+++ packages/desktop-editor/src/config/index.ts
@@ -0,0 +1,23 @@
+import audio from './audio.yaml';
+import buttons from './buttons.yaml';
+import characters from './characters.yaml';
+import common from './common.yaml';
+import items from './items.yaml';
+import quests from './quests.yaml';
+import screens from './screens.yaml';
+import skills from './skills.yaml';
+import skillChecks from './skillchecks.yaml';
+import { ModuleConfigInput } from 'narrat';
+
+const gameConfigs: ModuleConfigInput = {
+  audio,
+  buttons,
+  characters,
+  common,
+  items,
+  quests,
+  screens,
+  skills,
+  skillChecks,
+};
+export default gameConfigs;
