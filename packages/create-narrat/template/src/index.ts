09000719196481bf293cc89819190593929dd879
Liana P <32243840+liana-p@users.noreply.github.com>
2025-07-27 14:33:57 +0100

    css fix vite seems to have changed

--- packages/create-narrat/template/src/index.ts
+++ packages/create-narrat/template/src/index.ts
@@ -1,4 +1,4 @@
-import "narrat/dist/style.css";
+import "narrat/dist/main.css";
 import "./css/main.css";
 import { NarratPlugin, registerPlugin, startApp } from "narrat";
 import scripts from "./scripts";
