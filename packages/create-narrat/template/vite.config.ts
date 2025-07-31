f06af37cbf15bb6c4f6c27bf1177dd47ef927a85
Liana P <32243840+liana-p@users.noreply.github.com>
2025-07-31 09:19:47 +0100

    update some versions in the template

--- packages/create-narrat/template/vite.config.ts
+++ packages/create-narrat/template/vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig, UserConfigExport } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { fileURLToPath, URL } from "url";
-import { resolve } from "path";
 import Narrat from "vite-plugin-narrat";
+import packageJson from "./package.json";
+
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
+  process.env.VITE_BUILD_DATE = new Date().toLocaleString();
+  process.env.VITE_BUILD_VERSION = packageJson?.version ?? "Unknown";
+  process.env.VITE_GAME_NAME = packageJson?.name ?? "Unknown Game";
   const conf: UserConfigExport = {
     base: "",
+    optimizeDeps: {
+      exclude: ["steamworks.js"],
+    },
     resolve: {
       alias: {
         "@": fileURLToPath(new URL("./src", import.meta.url)),
